a07249a3417a74b51b4360d46d396dad071caed0
Greg Stula <gregstula@me.com>
2020-04-17 19:23:52 -0400

    Fix run.py script to account for differnt trials

---
+++ proj01/README.md
@@ -0,0 +1,3 @@
+#Project 01#
+Apologies for the delay stilll getting used to the new schedule.
+The XLXS and CVS files are provided a long with a run.py script and executable.
--- proj01/run.py
+++ proj01/run.py
@@ -1,3 +1,4 @@
+#!/bin/env python
 import os
 import sys
 import subprocess
@@ -10,15 +11,14 @@ print("Threads,NUMTRIALS,Probability,Performance")
 sys.stdout = original
 
 for t in [1,2,4,6,8,10,12,14,16]:
-    print("NUMT=%d" %t)
-    sys.stdout = f
-    trials = int(sys.argv[1])
-    cmd = "g++ -DNUMT=%d -DNUMTRIALS=%d main.cpp -o proj01 -lm -fopenmp" % (t, trials)
-    os.system(cmd)
-    cmd = "./proj01"
-    print(subprocess.run([cmd], stdout=subprocess.PIPE).stdout.decode('utf-8'), end = "")
-
-    # restore stdout
-    sys.stdout = original
+    for trials in [10,100,1000,10000]:
+        print("NUMT=%d" %t)
+        sys.stdout = f
+        cmd = "g++ -DNUMT=%d -DNUMTRIALS=%d main.cpp -o proj01 -lm -fopenmp" % (t, trials)
+        os.system(cmd)
+        cmd = "./proj01"
+        print(subprocess.run([cmd], stdout=subprocess.PIPE).stdout.decode('utf-8'), end = "")
+        # restore stdout
+        sys.stdout = original
 
 f.close()
