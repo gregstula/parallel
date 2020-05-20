1aef7140f12268d81e2ec4dd67b5d4cfc567ee86
Greg Stula <gregstula@me.com>
2020-05-20 18:02:10 -0400

    update python script

--- proj05/run.py
+++ proj05/run.py
@@ -10,15 +10,14 @@ sys.stdout = f
 print("threads,NUMNODES,average_area,performance")
 sys.stdout = original
 
-for t in [1,2,4,6,8,10,12,14,16]:
-    for nodes in [10,100,1000,10000]:
+for t in [16*1024, 32*1024, 64*1024, 128*1024, 256*1024, 512*1024, 1000*1024]:
+    for blck in [16,32,64,128]:
         print("NUMT=%d" %t)
         sys.stdout = f
-        cmd = "g++ -DNUMT=%d -DNUMNODES=%d main.cpp -o proj01 -lm -fopenmp" % (t, nodes)
+        cms = f"{cudaNVCC} -o proj05 CUDAmonteCarlo.cu -DNUMTRIALS={trial} -DBLOCKSIZE={blockSize}"
         os.system(cmd)
-        cmd = "./proj01"
+        cmd = "./proj05"
         print(subprocess.run([cmd], stdout=subprocess.PIPE).stdout.decode('utf-8'), end = "")
         # restore stdout
         sys.stdout = original
-
 f.close()
