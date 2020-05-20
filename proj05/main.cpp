52105e0c65c87191c4c2a926609df29a8f796bbf
Greg Stula <gregstula@me.com>
2020-05-20 17:56:27 -0400

    Add monte carlo code from proj1 and edit it

--- proj05/main.cpp
+++ proj05/main.cpp
@@ -94,85 +94,89 @@ int main(int argc, char *argv[]) {
   float current_prop;         // must be declared outside the NUMTRIES loop
 
   // looking for the maximum performance:
-    for (int t = 0; t < NUMTRIES; t++) {
+  for (int t = 0; t < NUMTRIES; t++) {
     double time0 = omp_get_wtime();
 
     int num_hits = 0;
 
-        #pragma omp parallel for default(none) shared(xcs,ycs,rs,tn) reduction(+:num_hits)
-        for (int n = 0; n < NUMTRIALS; n++) {
-             // randomize the location and radius of the circle:
-             float xc = xcs[n];
-             float yc = ycs[n];
-             float r = rs[n];
+#pragma omp parallel for default(none) shared(xcs,ycs,rs,tn) reduction(+:num_hits)
+    for (int n = 0; n < NUMTRIALS; n++) {
+      // randomize the location and radius of the circle:
+      float xc = xcs[n];
+      float yc = ycs[n];
+      float r = rs[n];
 
-             // solve for the intersection using the quadratic formula:
-             float a = 1. + tn * tn;
-             float b = -2. * (xc + yc * tn);
-             float c = xc * xc + yc * yc - r * r;
-             float d = b * b - 4. * a * c;
-             // If d is less than 0., then the circle was completely missed. (Case A)
-             // Continue on to the next trial in the for-loop.
+      // solve for the intersection using the quadratic formula:
+      float a = 1. + tn * tn;
+      float b = -2. * (xc + yc * tn);
+      float c = xc * xc + yc * yc - r * r;
+      float d = b * b - 4. * a * c;
+      // If d is less than 0., then the circle was completely missed. (Case A)
+      // Continue on to the next trial in the for-loop.
 
-             // hits the circle:
-             // get the first intersection:
-             d = sqrt(d);
-             if (d < 0) continue;
+      // hits the circle:
+      // get the first intersection:
+      d = sqrt(d);
+      if (d < 0)
+        continue;
 
-             float t1 = (-b + d) / (2. * a); // time to intersect the circle
-             float t2 = (-b - d) / (2. * a); // time to intersect the circle
-             float tmin = t1 < t2 ? t1 : t2; // only care about the first intersection
-             // If tmin is less than 0., then the circle completely engulfs the laser
-             // pointer. (Case B) Continue on to the next trial in the for-loop.
-             if (tmin < 0) continue;
+      float t1 = (-b + d) / (2. * a); // time to intersect the circle
+      float t2 = (-b - d) / (2. * a); // time to intersect the circle
+      float tmin = t1 < t2 ? t1 : t2; // only care about the first intersection
+      // If tmin is less than 0., then the circle completely engulfs the laser
+      // pointer. (Case B) Continue on to the next trial in the for-loop.
+      if (tmin < 0)
+        continue;
 
-             // where does it intersect the circle?
-             float xcir = tmin;
-             float ycir = tmin * tn;
+      // where does it intersect the circle?
+      float xcir = tmin;
+      float ycir = tmin * tn;
 
-             // get the unitized normal vector at the point of intersection:
-             float nx = xcir - xc;
-             float ny = ycir - yc;
-             float nxy = sqrt(nx * nx + ny * ny);
-             nx /= nxy; // unit vector
-             ny /= nxy; // unit vector
+      // get the unitized normal vector at the point of intersection:
+      float nx = xcir - xc;
+      float ny = ycir - yc;
+      float nxy = sqrt(nx * nx + ny * ny);
+      nx /= nxy; // unit vector
+      ny /= nxy; // unit vector
 
-             // get the unitized incoming vector:
-             float inx = xcir - 0.;
-             float iny = ycir - 0.;
-             float in = sqrt(inx * inx + iny * iny);
-             inx /= in; // unit vector
-             iny /= in; // unit vector
+      // get the unitized incoming vector:
+      float inx = xcir - 0.;
+      float iny = ycir - 0.;
+      float in = sqrt(inx * inx + iny * iny);
+      inx /= in; // unit vector
+      iny /= in; // unit vector
 
-             // get the outgoing (bounced) vector:
-             float dot = inx * nx + iny * ny;
-             float outx = inx - 2. * nx * dot; // angle of reflection = angle of incidence`
-             float outy = iny - 2. * ny * dot; // angle of reflection = angle of incidence`
+      // get the outgoing (bounced) vector:
+      float dot = inx * nx + iny * ny;
+      float outx =
+          inx - 2. * nx * dot; // angle of reflection = angle of incidence`
+      float outy =
+          iny - 2. * ny * dot; // angle of reflection = angle of incidence`
 
-             // find out if it hits the infinite plate:
-             float tt = (0. - ycir) / outy;
+      // find out if it hits the infinite plate:
+      float tt = (0. - ycir) / outy;
 
-             // If tt is less than 0., then the reflected beam went up instead of down.
-             // Continue on to the next trial in the for-loop.
+      // If tt is less than 0., then the reflected beam went up instead of down.
+      // Continue on to the next trial in the for-loop.
 
-             // Otherwise, this beam hit the infinite plate. (Case D) Increment the
-             // number of hits and continue on to the next trial in the for-loop.
-             if (tt > 0)  {
-               num_hits++;
-             }
-        }
+      // Otherwise, this beam hit the infinite plate. (Case D) Increment the
+      // number of hits and continue on to the next trial in the for-loop.
+      if (tt > 0) {
+        num_hits++;
+      }
+    }
 
-        double time1 = omp_get_wtime();
-        double mega_trials_per_second = (double)NUMTRIALS / (time1 - time0) / 1000000.;
+    double time1 = omp_get_wtime();
+    double mega_trials_per_second =
+        (double)NUMTRIALS / (time1 - time0) / 1000000.;
 
-       if (mega_trials_per_second > max_performance) {
-            max_performance = mega_trials_per_second;
-       }
+    if (mega_trials_per_second > max_performance) {
+      max_performance = mega_trials_per_second;
+    }
 
-       current_prop = (float)num_hits / (float)NUMTRIALS;
-     }
-    // Output csv with threads, trials, probability, and performance
-    printf("%d,%d,%lf,%lf\n", NUMT, NUMTRIALS, current_prop, max_performance);
-    return 0;
+    current_prop = (float)num_hits / (float)NUMTRIALS;
+  }
+  // Output csv with threads, trials, probability, and performance
+  printf("%d,%d,%lf,%lf\n", NUMT, NUMTRIALS, current_prop, max_performance);
+  return 0;
 }
-
--- proj05/project5.cu
+++ proj05/project5.cu
@@ -1,262 +1,272 @@
 // System includes
-#include <stdio.h>
 #include <assert.h>
 #include <malloc.h>
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 // CUDA runtime
 #include <cuda_runtime.h>
 
 // Helper functions and utilities to work with CUDA
-#include "helper_functions.h"
 #include "helper_cuda.h"
-
+#include "helper_functions.h"
 
 // setting the number of trials in the monte carlo simulation:
 #ifndef NUMTRIALS
-#define NUMTRIALS	( 1024*1024 )
+#define NUMTRIALS (1024 * 1024)
 #endif
-
 
 #ifndef BLOCKSIZE
-#define BLOCKSIZE		32     // number of threads per block
+#define BLOCKSIZE 32 // number of threads per block
 #endif
 
-#define NUMBLOCKS		( NUMTRIALS / BLOCKSIZE )
-
+#define NUMBLOCKS (NUMTRIALS / BLOCKSIZE)
 
 // ranges for the random numbers:
-const float XCMIN =	 0.0;
-const float XCMAX =	 2.0;
-const float YCMIN =	 0.0;
-const float YCMAX =	 2.0;
-const float RMIN  =	 0.5;
-const float RMAX  =	 2.0;
+const float XCMIN = 0.0;
+const float XCMAX = 2.0;
+const float YCMIN = 0.0;
+const float YCMAX = 2.0;
+const float RMIN = 0.5;
+const float RMAX = 2.0;
 
 // function prototypes:
-float		Ranf( float, float );
-int		Ranf( int, int );
-void		TimeOfDaySeed( );
+float Ranf(float, float);
+int Ranf(int, int);
+void TimeOfDaySeed();
 
+__global__ void MonteCarlo(float *Xcs, float *Ycs, float *Rs, int *Hits) {
+  unsigned int wgNumber = blockIdx.x;
+  unsigned int wgDimension = blockDim.x;
+  unsigned int threadNum = threadIdx.x;
+  unsigned int gid = wgNumber * wgDimension + threadNum;
 
+  // all the monte carlo stuff goes in here
+  // if we make it all the way through, then Hits[gid] = 1
 
-__global__  void MonteCarlo( float *Xcs, float *Ycs, float *Rs, int *Hits )
-{
-	unsigned int wgNumber      = blockIdx.x;
-	unsigned int wgDimension   = blockDim.x;
-	unsigned int threadNum     = threadIdx.x;
-	unsigned int gid           = wgNumber*wgDimension + threadNum;
+  // randomize the location and radius of the circle:
+  float xc = Xcs[gid];
+  float yc = Ycs[gid];
+  float r = Rs[gid];
 
-	// all the monte carlo stuff goes in here
-	// if we make it all the way through, then Hits[gid] = 1
+  float tn = tanf((float)((M_PI / 180.) * 30.));
+  Hits[gid] = 0;
 
-	// randomize the location and radius of the circle:
-	float xc = Xcs[gid];
-	float yc = Ycs[gid];
-	float  r =  Rs[gid];
+  // solve for the intersection using the quadratic formula:
 
-	float tn = tanf( (float)( (M_PI/180.) * 30. ) );
-	Hits[gid] = 0;
+  float a = 1. + tn * tn;
+  float b = -2. * (xc + yc * tn);
+  float c = xc * xc + yc * yc - r * r;
+  float d = b * b - 4. * a * c;
 
-	// solve for the intersection using the quadratic formula:
+  // cascading if-statements:
+  //	if you used "continue;" in project #1, change to this style because,
+  //	if there is no for-loop, then there is nowhere to continue to
 
-	float a = . . .
-	float b = . . .
-	float c = . . .
-	float d = . . .
+  if (d >= 0) {
+    d = sqrt(d);
+    float t1 = (-b + d) / (2. * a); // time to intersect the circle
+    float t2 = (-b - d) / (2. * a); // time to intersect the circle
+    float tmin = t1 < t2 ? t1 : t2; // only care about the first intersection
 
-	// cascading if-statements:
-	//	if you used "continue;" in project #1, change to this style because,
-	//	if there is no for-loop, then there is nowhere to continue to
+    if (tmin >= 0) {
+      // where does it intersect the circle?
+      float xcir = tmin;
+      float ycir = tmin * tn;
 
-	if( ... )
-	{
-		. . .
+      // get the unitized normal vector at the point of intersection:
+      float nx = xcir - xc;
+      float ny = ycir - yc;
+      float nxy = sqrt(nx * nx + ny * ny);
+      nx /= nxy; // unit vector
+      ny /= nxy; // unit vector
 
-		if( ... )
-		{
-			. . .
+      // get the unitized incoming vector:
+      float inx = xcir - 0.;
+      float iny = ycir - 0.;
+      float in = sqrt(inx * inx + iny * iny);
+      inx /= in; // unit vector
+      iny /= in; // unit vector
 
-			// find out if it hits the infinite plate:
-			float t = ( 0. - ycir ) / outy;
-			if( t >= 0. )
-			{
-				Hits[gid] = 1;
-			}
-		}
-	}
+      // get the outgoing (bounced) vector:
+      float dot = inx * nx + iny * ny;
+      float outy =
+          iny - 2. * ny * dot; // angle of reflection = angle of incidence`
+
+      // find out if it hits the infinite plate:
+      float t = (0. - ycir) / outy;
+      if (t >= 0.) {
+        Hits[gid] = 1;
+      }
+    }
+  }
 }
-
 
 // main program:
 
-int
-main( int argc, char* argv[ ] )
-{
-	TimeOfDaySeed( );
+int main(int argc, char *argv[]) {
+  TimeOfDaySeed();
 
-	int dev = findCudaDevice(argc, (const char **)argv);
+  int dev = findCudaDevice(argc, (const char **)argv);
 
-	// allocate host memory:
+  // allocate host memory:
 
-	float *hXcs  = new float[NUMTRIALS];
-	float *hYcs  = new float[NUMTRIALS];
-	float * hRs  = new float[NUMTRIALS];
-	int   *hHits = new   int[NUMTRIALS];
+  float *hXcs = new float[NUMTRIALS];
+  float *hYcs = new float[NUMTRIALS];
+  float *hRs = new float[NUMTRIALS];
+  int *hHits = new int[NUMTRIALS];
 
-	// fill the random-value arrays:
-	for( int n = 0; n < NUMTRIALS; n++ )
-	{
-		hXcs[n] = Ranf( XCMIN, XCMAX );
-		hYcs[n] = Ranf( YCMIN, YCMAX );
- 		hRs[n]  = Ranf(  RMIN,  RMAX );
-	}
+  // fill the random-value arrays:
+  for (int n = 0; n < NUMTRIALS; n++) {
+    hXcs[n] = Ranf(XCMIN, XCMAX);
+    hYcs[n] = Ranf(YCMIN, YCMAX);
+    hRs[n] = Ranf(RMIN, RMAX);
+  }
 
-	// allocate device memory:
+  // allocate device memory:
 
-	float *dXcs, *dYcs, *dRs;
-	int *dHits;
+  float *dXcs, *dYcs, *dRs;
+  int *dHits;
 
-	dim3 dimsXcs(  NUMTRIALS, 1, 1 );
-	dim3 dimsYcs(  NUMTRIALS, 1, 1 );
-	dim3 dimsRs(   NUMTRIALS, 1, 1 );
-	dim3 dimsHits( NUMTRIALS, 1, 1 );
+  dim3 dimsXcs(NUMTRIALS, 1, 1);
+  dim3 dimsYcs(NUMTRIALS, 1, 1);
+  dim3 dimsRs(NUMTRIALS, 1, 1);
+  dim3 dimsHits(NUMTRIALS, 1, 1);
 
+  cudaError_t status;
+  status = cudaMalloc((void **)(&dXcs), NUMTRIALS * sizeof(float));
+  checkCudaErrors(status);
 
-	cudaError_t status;
-	status = cudaMalloc( (void **)(&dXcs), NUMTRIALS*sizeof(float) );
-	checkCudaErrors( status );
+  status = cudaMalloc((void **)(&dYcs), NUMTRIALS * sizeof(float));
+  checkCudaErrors(status);
 
-	status = cudaMalloc( (void **)(&dYcs), NUMTRIALS*sizeof(float) );
-	checkCudaErrors( status );
+  status = cudaMalloc((void **)(&dRs), NUMTRIALS * sizeof(float));
+  checkCudaErrors(status);
 
-	status = cudaMalloc( (void **)(&dRs), NUMTRIALS*sizeof(float) );
-	checkCudaErrors( status );
+  status = cudaMalloc((void **)(&dHits), NUMTRIALS * sizeof(int));
+  checkCudaErrors(status);
 
-	status = cudaMalloc( (void **)(&dHits), NUMTRIALS *sizeof(int) );
-	checkCudaErrors( status );
+  // copy host memory to the device:
 
+  status =
+      cudaMemcpy(dXcs, hXcs, NUMTRIALS * sizeof(float), cudaMemcpyHostToDevice);
+  checkCudaErrors(status);
 
-	// copy host memory to the device:
+  status =
+      cudaMemcpy(dYcs, hYcs, NUMTRIALS * sizeof(float), cudaMemcpyHostToDevice);
+  checkCudaErrors(status);
 
-	status = cudaMemcpy( dXcs, hXcs, NUMTRIALS*sizeof(float), cudaMemcpyHostToDevice );
-	checkCudaErrors( status );
+  status =
+      cudaMemcpy(dRs, hRs, NUMTRIALS * sizeof(float), cudaMemcpyHostToDevice);
+  checkCudaErrors(status);
 
-	status = cudaMemcpy( dYcs, hYcs, NUMTRIALS*sizeof(float), cudaMemcpyHostToDevice );
-	checkCudaErrors( status );
+  // setup the execution parameters:
 
-	status = cudaMemcpy( dRs, hRs, NUMTRIALS*sizeof(float), cudaMemcpyHostToDevice );
-	checkCudaErrors( status );
+  dim3 threads(BLOCKSIZE, 1, 1);
+  dim3 grid(NUMBLOCKS, 1, 1);
 
+  // create and start timer
 
-	// setup the execution parameters:
+  cudaDeviceSynchronize();
 
-	dim3 threads(BLOCKSIZE, 1, 1 );
-	dim3 grid(NUMBLOCKS, 1, 1 );
+  // allocate CUDA events that we'll use for timing:
 
-	// create and start timer
+  cudaEvent_t start, stop;
+  status = cudaEventCreate(&start);
+  checkCudaErrors(status);
+  status = cudaEventCreate(&stop);
+  checkCudaErrors(status);
 
-	cudaDeviceSynchronize( );
+  // record the start event:
 
-	// allocate CUDA events that we'll use for timing:
+  status = cudaEventRecord(start, NULL);
+  checkCudaErrors(status);
 
-	cudaEvent_t start, stop;
-	status = cudaEventCreate( &start );
-	checkCudaErrors( status );
-	status = cudaEventCreate( &stop );
-	checkCudaErrors( status );
+  // execute the kernel:
 
-	// record the start event:
+  MonteCarlo<<<grid, threads>>>(dXcs, dYcs, dRs, dHits);
 
-	status = cudaEventRecord( start, NULL );
-	checkCudaErrors( status );
+  // record the stop event:
 
-	// execute the kernel:
+  status = cudaEventRecord(stop, NULL);
+  checkCudaErrors(status);
 
-	MonteCarlo<<< grid, threads >>>( dXcs, dYcs, dRs, dHits );
+  // wait for the stop event to complete:
 
-	// record the stop event:
+  status = cudaEventSynchronize(stop);
+  checkCudaErrors(status);
 
-	status = cudaEventRecord( stop, NULL );
-	checkCudaErrors( status );
+  float msecTotal = 0.0f;
+  status = cudaEventElapsedTime(&msecTotal, start, stop);
+  checkCudaErrors(status);
 
-	// wait for the stop event to complete:
+  // compute and print the performance
 
-	status = cudaEventSynchronize( stop );
-	checkCudaErrors( status );
+  double secondsTotal = 0.001 * (double)msecTotal;
+  double trialsPerSecond = (float)NUMTRIALS / secondsTotal;
+  double megaTrialsPerSecond = trialsPerSecond / 1000000.;
+  fprintf(stderr, "Number of Trials = %10d, MegaTrials/Second = %10.4lf\n",
+          NUMTRIALS, megaTrialsPerSecond);
 
-	float msecTotal = 0.0f;
-	status = cudaEventElapsedTime( &msecTotal, start, stop );
-	checkCudaErrors( status );
+  // copy result from the device to the host:
 
-	// compute and print the performance
+  status =
+      cudaMemcpy(hHits, dHits, NUMTRIALS * sizeof(int), cudaMemcpyDeviceToHost);
+  checkCudaErrors(status);
+  cudaDeviceSynchronize();
 
-	double secondsTotal = 0.001 * (double)msecTotal;
-	double trialsPerSecond = (float)NUMTRIALS / secondsTotal;
-	double megaTrialsPerSecond = trialsPerSecond / 1000000.;
-	fprintf( stderr, "Number of Trials = %10d, MegaTrials/Second = %10.4lf\n", NUMTRIALS, megaTrialsPerSecond );
+  // compute the probability:
 
-	// copy result from the device to the host:
+  int numHits = 0;
+  for (int i = 0; i < NUMTRIALS; i++) {
+    numHits += hHits[i];
+  }
 
-	status = cudaMemcpy( hHits, dHits, NUMTRIALS *sizeof(int), cudaMemcpyDeviceToHost );
-	checkCudaErrors( status );
-	cudaDeviceSynchronize( );
+  float probability = 100.f * (float)numHits / (float)NUMTRIALS;
+  fprintf(stderr, "\nProbability = %6.3f %%\n", probability);
 
-	// compute the probability:
+  // clean up memory:
+  delete[] hXcs;
+  delete[] hYcs;
+  delete[] hRs;
+  delete[] hHits;
 
-	int numHits = 0;
-	for(int i = 0; i < NUMTRIALS; i++ )
-	{
-		numHits += hHits[i];
-	}
+  status = cudaFree(dXcs);
+  status = cudaFree(dYcs);
+  status = cudaFree(dRs);
+  status = cudaFree(dHits);
+  checkCudaErrors(status);
 
-	float probability = 100.f * (float)numHits / (float)NUMTRIALS;
-	fprintf(stderr, "\nProbability = %6.3f %%\n", probability );
-
-	// clean up memory:
-	delete [ ] hXcs;
-	delete [ ] hYcs;
-	delete [ ] hRs;
-	delete [ ] hHits;
-
-	status = cudaFree( dXcs );
-	status = cudaFree( dYcs );
-	status = cudaFree( dRs );
-	status = cudaFree( dHits );
-	checkCudaErrors( status );
-
-	return 0;
+  return 0;
 }
 
-float
-Ranf( float low, float high )
-{
-	float r = (float) rand();               // 0 - RAND_MAX
-	float t = r  /  (float) RAND_MAX;       // 0. - 1.
+float Ranf(float low, float high) {
+  float r = (float)rand();       // 0 - RAND_MAX
+  float t = r / (float)RAND_MAX; // 0. - 1.
 
-	return   low  +  t * ( high - low );
+  return low + t * (high - low);
 }
 
-int
-Ranf( int ilow, int ihigh )
-{
-	float low = (float)ilow;
-	float high = ceil( (float)ihigh );
+int Ranf(int ilow, int ihigh) {
+  float low = (float)ilow;
+  float high = ceil((float)ihigh);
 
-	return (int) Ranf(low,high);
+  return (int)Ranf(low, high);
 }
 
-void
-TimeOfDaySeed( )
-{
-	struct tm y2k = { 0 };
-	y2k.tm_hour = 0;   y2k.tm_min = 0; y2k.tm_sec = 0;
-	y2k.tm_year = 100; y2k.tm_mon = 0; y2k.tm_mday = 1;
+void TimeOfDaySeed() {
+  struct tm y2k = {0};
+  y2k.tm_hour = 0;
+  y2k.tm_min = 0;
+  y2k.tm_sec = 0;
+  y2k.tm_year = 100;
+  y2k.tm_mon = 0;
+  y2k.tm_mday = 1;
 
-	time_t  timer;
-	time( &timer );
-	double seconds = difftime( timer, mktime(&y2k) );
-	unsigned int seed = (unsigned int)( 1000.*seconds );    // milliseconds
-	srand( seed );
+  time_t timer;
+  time(&timer);
+  double seconds = difftime(timer, mktime(&y2k));
+  unsigned int seed = (unsigned int)(1000. * seconds); // milliseconds
+  srand(seed);
 }
